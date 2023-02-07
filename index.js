b68b191e8e637e281e9466906648bcab09c2f388
juliansilveira <julian.silveira.annoni@gmail.com>
2023-02-07 17:02:33 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let linkedin = "https://www.linkedin.com/in/julian-sa/"
+
+let descripcion = "Me presento, soy Julián silveira annoni, soy de Concordia Entre Ríos. Nacido el 08/09/1995"
+
+let github = "https://github.com/juliansilveira"
+
+let contacto = "Mail : julian.silveira.annoni@gmail.com <br> Teléfono : 0345 - 4168370"
+
+
+
+function mostrar(varia) {
+    document.getElementById("descripcion").innerHTML = varia
+}
